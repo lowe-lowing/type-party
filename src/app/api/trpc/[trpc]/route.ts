87b8dc257d8ede451385cf5914aa2d436d9617d1
Lowe Löwing <lowe.lowing@gmail.com>
2023-09-06 15:06:44 +0200

    trpc runtime edge

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,14 +1,15 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/lib/trpc/server/router/_app";
+import { createContext } from "@/lib/trpc/context";
 
-// export const runtime = "edge";
+export const runtime = "edge";
 
 const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: () => ({}),
+    createContext,
   });
 
 export { handler as GET, handler as POST };
